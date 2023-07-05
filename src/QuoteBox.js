ce1f6551a84c318db7c5d3a0062c0ab6cb1509dc
waifu.dev <waifu_dev@waifu.dev>
2023-07-04 21:40:09 -0700

    first commit

---
+++ src/QuoteBox.js
@@ -0,0 +1,67 @@
+import React, {useState, useEffect} from 'react';
+import { IoMdQuote } from 'react-icons/io';
+import { AiFillTwitterSquare } from 'react-icons/ai';
+
+
+/**
+ * Quotation card
+ * Powered by the Quotable API
+ * https://github.com/lukePeavey/quotable
+ */
+
+
+const QuoteBox = () => {
+
+    const [quote, setQuote] = useState("");
+    const [author, setAutor] = useState("");
+    const twitterLink = "http://www.twitter.com/intent/tweet?text=" + quote + "&hashtags=" + author.replace(/\s+/g, '');
+
+    /* Twitter Icon btn styling */
+    
+    const style = { 
+        color: "#CECECE",
+        height: "100%",
+        width: "250%",
+        background: "#48515D"
+    };
+    
+    
+    const handleClick = () => {
+        console.log('New Quote Button Clicked');
+
+        fetch("https://api.quotable.io/random")
+        .then(res => res.json()) 
+        .then(
+            (quote) => {
+            setQuote(quote.content);
+            setAutor(quote.author);
+            }
+      )
+    }
+
+    useEffect( () => {
+        fetch("https://api.quotable.io/random")
+          .then(res => res.json()) 
+          .then(
+            (quote) => {
+              setQuote(quote.content);
+              setAutor(quote.author);
+            }
+          )
+      },[]);
+
+    return (  
+        <div className="quotebox" id="quote-box">
+            <p id="text"> <IoMdQuote /> { quote }</p>
+            <p id="author">- { author }</p>
+            <div className="buttons">
+                <a id="tweet-quote" target="_blank" href={twitterLink}>
+                    <AiFillTwitterSquare className="tweetQuote" />
+                </a>
+                <button id="new-quote" onClick={handleClick}>New Quote</button>
+            </div>
+        </div>
+    );
+}
+ 
+export default QuoteBox;
